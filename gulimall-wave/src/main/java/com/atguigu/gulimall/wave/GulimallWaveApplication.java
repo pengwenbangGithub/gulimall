fb7652caef6eead4c8bd46338772dcebcdf26c5f
pengwenbangGithub <504002676@qq.com>
2024-03-09 10:23:36 +0800

    项目初始结构创建

---
+++ gulimall-wave/src/main/java/com/atguigu/gulimall/wave/GulimallWaveApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.wave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallWaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallWaveApplication.class, args);
+    }
+
+}
